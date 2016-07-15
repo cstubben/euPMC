ca7acafbac16c37bc0cfef4135ac0bf7f0745433
Chris Stubben <chris.stubben@gmail.com>
2016-07-14 18:19:07 -0600

    add search_core

--- R/format_mesh.R
+++ R/format_mesh.R
@@ -4,8 +4,12 @@
 
 format_mesh<-function( x){
    if(is.data.frame(x)) x<- x$mesh
+
    y <- unlist( strsplit(x, "; "))
    y<- y[y!="NA"]
+if(length(y)==0){
+   y<-NA
+}else{
    z<- ifelse(grepl("\\*", y), "Major", "Total")
    y <- gsub("\\*", "", y)
    y <- as.data.frame.matrix( table(y,z) )
@@ -16,5 +20,8 @@ format_mesh<-function( x){
    y <- data.frame(MeSH = rownames(y), y, stringsAsFactors=FALSE)
    rownames(y) <- NULL
    y[order(y$Major, y$Total, decreasing=TRUE),]
+}
+y
+
 }
 
